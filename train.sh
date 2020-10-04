7989efa7ec8948ab74fde5c17fc96aa807040de2
n1ckfg <nick@fox-gieg.com>
2020-10-03 21:31:06 -0400

    shell scripts

---
+++ train.sh
@@ -0,0 +1,4 @@
+python main2.py --dataroot /data1/cuiqingli/data/render_v5/denseposeV4_512 --sample_st 1 --sample_ed 80 --num_epochs 10 \
+--batch_size 2 --frame_count 11 --resize_w 512 --resize_h 512 --mode train --lr 0.0002 --model_name model512_denseposeV4_mse_0611 --gpu_ids 0 
+
+
