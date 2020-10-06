041d136d37b202a045b4618afee80e7636529068
n1ckfg <nick@fox-gieg.com>
2020-10-06 13:11:50 -0400

    shell scripts

--- test.sh
+++ test.sh
@@ -1,7 +1,9 @@
 INPUT_DIR=files/input
 MODEL_DIR=outputmodel
+IMAGE_SIZE=512
+GPU_ID=0
 
 python main2.py --dataroot $INPUT_DIR --sample_st 0 --sample_ed 1 \
---batch_size 2 --frame_count 11 --resize_w 256 --resize_h 256 --mode test --checkpoint_name /data1/cuiqingli/Pix2PixHD-TensorFlow/checkpoints/model256_denseposeV4_mse --gpu_ids 6 \
+--batch_size 2 --frame_count 11 --resize_w $IMAGE_SIZE --resize_h $IMAGE_SIZE --mode test --checkpoint_name /data1/cuiqingli/Pix2PixHD-TensorFlow/checkpoints/model256_denseposeV4_mse --gpu_ids $GPU_ID \
 --epoch_id_inference 6 --model_name $MODEL_DIR --need_resize True --crop_h_flag 20
 
--- train.sh
+++ train.sh
@@ -1,8 +1,11 @@
 TRAIN_DIR=files/train
 MODEL_DIR=outputmodel
+IMAGE_SIZE=512
+GPU_ID=0
 
 python main2.py --dataroot $TRAIN_DIR --sample_st 1 --sample_ed 80 --num_epochs 10 \
---batch_size 2 --frame_count 11 --resize_w 512 --resize_h 512 --mode train --lr 0.0002 --model_name $MODEL_DIR --gpu_ids 0
+--batch_size 2 --frame_count 11 --resize_w $IMAGE_SIZE --resize_h $IMAGE_SIZE --mode train --lr 0.0002 --model_name $MODEL_DIR --gpu_ids $GPU_ID
+
  
 
 
