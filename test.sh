0868e0f8609d06be9125c0713ef3dc2943b03870
n1ckfg <nick@fox-gieg.com>
2020-10-06 13:04:56 -0400

    shell scripts

--- test.sh
+++ test.sh
@@ -1,4 +1,7 @@
-python main2.py --dataroot /data1/cuiqingli/app4_v2_1sec/result --sample_st 0 --sample_ed 1 \
+INPUT_DIR=files/input
+MODEL_DIR=outputmodel
+
+python main2.py --dataroot $INPUT_DIR --sample_st 0 --sample_ed 1 \
 --batch_size 2 --frame_count 11 --resize_w 256 --resize_h 256 --mode test --checkpoint_name /data1/cuiqingli/Pix2PixHD-TensorFlow/checkpoints/model256_denseposeV4_mse --gpu_ids 6 \
---epoch_id_inference 6 --model_name model256_dneseposeV4_app4_v2_lsec --need_resize True --crop_h_flag 20
+--epoch_id_inference 6 --model_name $MODEL_DIR --need_resize True --crop_h_flag 20
 
--- train.sh
+++ train.sh
@@ -1,4 +1,8 @@
-python main2.py --dataroot /data1/cuiqingli/data/render_v5/denseposeV4_512 --sample_st 1 --sample_ed 80 --num_epochs 10 \
---batch_size 2 --frame_count 11 --resize_w 512 --resize_h 512 --mode train --lr 0.0002 --model_name model512_denseposeV4_mse_0611 --gpu_ids 0 
+TRAIN_DIR=files/train
+MODEL_DIR=outputmodel
+
+python main2.py --dataroot $TRAIN_DIR --sample_st 1 --sample_ed 80 --num_epochs 10 \
+--batch_size 2 --frame_count 11 --resize_w 512 --resize_h 512 --mode train --lr 0.0002 --model_name $MODEL_DIR --gpu_ids 0
+ 
 
 
